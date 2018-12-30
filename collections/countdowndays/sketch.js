829d990bf0bd7f7ae0779250e5b2c2420a6a429c
Mahmoud <mhd-10@hotmail.com>
2018-12-30 11:38:46 +0300

    js html css

--- collections/countdowndays/sketch.js
+++ collections/countdowndays/sketch.js
@@ -1,43 +1,74 @@
-counter = 0
-timeIt = 0
+let goal = new Date("2019-01-01")
+let timenow = new Date()
+days = null 
+hrs= null
+mins= null
+sec=null
+// counter = 0
+// timeIt = 0
 
 
-timeleft = 20
+// timeleft = 20
 
 
-function convertsecond(s){
-var m = minute();
-var h = hour();
-var s = second();
-	sec = s%60;
-	hr = min * 60
-	return text('Current minute: \n' + m, 5, 50);
-	text('Current hour:\n' + s, 5, 50);
+// function convertsecond(s){
+// var m = minute();
+// var h = hour();
+// var s = second();
+// 	sec = s%60;
+// 	hr = min * 60
+// 	return text('Current minute: \n' + m, 5, 50);
+// 	text('Current hour:\n' + s, 5, 50);
 	
-text('Current hour:\n' + h, 5, 50);
+// text('Current hour:\n' + h, 5, 50);
 	
 	
 	//nf(hr,2) + ':' + nf (min,2 ) + ':' + nf(sec,2);
-}
+// }
 
 
 
 function setup() {
+	let countdown = select(".countdown")
+	countdown.position(windowWidth/2-200,windowHeight/2-200)
+	let goal = new Date("2019-01-01")
+	let timenow = new Date()
+	let result = goal-timenow
+	 days = Math.floor((result)/(1000*60*60*24))
+	 hrs = Math.floor ((result/ (1000*60*60))%24)
+	 mins = Math.floor ((result/ (1000*60))%60)
+	 sec = Math.floor ((result/ (1000))%60)
+	 days = days > 0 ? days : 0
+	 hrs = hrs > 0 ? hrs : 0
+	 mins = mins > 0 ? mins : 0
+	 sec = sec > 0 ? sec : 0
 
 
-	var timer = select('#timer')
+	// var timer = select('#timer')
+	// timer.html(convertsecond(timeleft - counter))
+
+
+
+
+}
+function timeIt() {
+	counter++
 	timer.html(convertsecond(timeleft - counter))
-
-
-	function timeIt() {
-		counter++
-		timer.html(convertsecond(timeleft - counter))
-		if (timeleft<=0){
-			counter =0 
-		}
+	if (timeleft<=0){
+		counter =0 
 	}
+}
 
 
-	setInterval(timeIt, 1000)
+ setInterval(setup, 1000)
 
+function draw(){
+t = select ("#days")
+t.html(days)
+t = select ("#hrs")
+t.html(hrs)
+t = select ("#mins")
+t.html(mins)
+t = select ("#sec")
+t.html(sec)
 }
